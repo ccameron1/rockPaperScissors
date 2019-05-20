e43d77f6d193036a1f84f49354ae83cf00b0ca43
Carly Cameron <carlycameron01@gmail.com>
2019-05-20 13:11:14 -0500

    (add imageViews)

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var choice : UIImageView?
+    @IBOutlet weak var imageViewStack: UIStackView!
+    @IBOutlet var imageViews: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+        let fvc = segue.destination as! FinalViewController
+     }
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        
+        let selectedLabel = sender.location(in: imageViewStack)
+        for image in imageViews {
+            if image.frame.contains(selectedLabel) {
+                choice = image
+                
+            }
+        }
+        print(choice)
+        
+    }
+    
 }
 
